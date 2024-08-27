6589f90b75f17754572f4693b85a5f752cc76f8f
wood <wood13590@gmail.com>
2024-08-27 20:26:29 +0800

    initial 测试网关

---
+++ src/main/java/com/wood/woodapigateway/WoodapiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.wood.woodapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodapiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoodapiGatewayApplication.class, args);
+    }
+}
---
+++ src/test/java/com/wood/woodapigateway/WoodapiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wood.woodapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoodapiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
